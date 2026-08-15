f82a0c7b4142dc63c0eb228d8e1f5bd3b200d9a0
kunalAndrDev <kunalgarode09@gmail.com>
2026-08-15 16:15:28 +0530

    my Camp Frontend Done

--- app/src/main/java/com/example/hcare/Home_Screen.java
+++ app/src/main/java/com/example/hcare/Home_Screen.java
@@ -8,16 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Home_Screen extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_home_screen);
 
-
     }
+
 }
