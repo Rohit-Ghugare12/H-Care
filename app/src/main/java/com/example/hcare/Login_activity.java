42da953419352a9b6a42aa9c330ea262bd22ab25
Rohit <rghugare720@gmail.com>
2026-08-15 17:40:08 +0600

    Login Backend

--- app/src/main/java/com/example/hcare/Login_activity.java
+++ app/src/main/java/com/example/hcare/Login_activity.java
@@ -3,7 +3,9 @@ package com.example.hcare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,7 @@ public class Login_activity extends AppCompatActivity {
 
     TextView tvForgetPassword,tvRegisterhere;
     AppCompatButton btnLogin;
+    EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,8 @@ public class Login_activity extends AppCompatActivity {
         tvForgetPassword = findViewById(R.id.tvForgetPassword);
         tvRegisterhere = findViewById(R.id.tvRegisterhere);
         btnLogin = findViewById(R.id.btnLogin);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
 
         tvRegisterhere.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,9 +39,33 @@ public class Login_activity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (etUsername.getText().toString().isEmpty())
+                {
+                    etUsername.setError("Enter Username");
+                }
+                else if (etPassword.getText().toString().isEmpty()) {
 
-                Intent intent = new Intent(Login_activity.this, Home_Screen.class);
-                startActivity(intent);
+                    etPassword.setError( "Enter Password");
+                } else if ( etPassword.getText().toString().length()<=8)
+                {
+                    etPassword.setError( " Password length must be greater than 8");
+
+                } else if (!etPassword.getText().toString().matches(".*[A-Z].*")) {
+
+                    etPassword.setError(  "Password matches 1 Number,1 Symbol,1 Upper case latter,1 Lower case letter");
+                } else if (!etPassword.getText().toString().matches(".*[@,#,$,%,!,*,&,~,%].*")) {
+
+                    etPassword.setError(  "Password matches 1 Number,1 Symbol,1 Upper case latter,1 Lower case letter");
+                } else if (!etPassword.getText().toString().matches(".*[0-9].*")) {
+                    etPassword.setError(  "Password matches 1 Number,1 Symbol,1 Upper case latter,1 Lower case letter");
+                }else if (!etPassword.getText().toString().matches(".*[a-z].*"))
+                {
+                    etPassword.setError(  "Password matches 1 Number,1 Symbol,1 Upper case latter,1 Lower case letter");
+                }else
+                {
+                    Intent intent = new Intent(Login_activity.this, Home_Screen.class);
+                    startActivity(intent);
+                }
             }
         });
 
