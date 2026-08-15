a6d10765bdbaeeefb29b64836de6d1bae75a1ba2
Rohit <rghugare720@gmail.com>
2026-08-15 16:23:20 +0600

    Changes By Rohit

--- app/src/main/java/com/example/hcare/Home_Screen.java
+++ app/src/main/java/com/example/hcare/Home_Screen.java
@@ -1,8 +1,14 @@
 package com.example.hcare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,6 +28,45 @@ public class Home_Screen extends AppCompatActivity {
 
         setContentView(R.layout.activity_home_screen);
 
+
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuHomeAbout)
+        {
+            Toast.makeText(this, "About Us Clicked", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.getItemId() == R.id.menuHomeContact)
+        {
+
+        }
+        else if (item.getItemId() == R.id.menuHomeLogOut )
+        {
+
+            Intent intent = new Intent(Home_Screen.this,Login_activity.class);
+            startActivity(intent);
+            finishAffinity();
+
+        }
+        else if (item.getItemId() == R.id.menuHomeSetting )
+        {
+            Toast.makeText(this, "Setting Clicked", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.getItemId() == R.id.menuHomeMap)
+        {
+            Toast.makeText(this, "Map Clicked", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
