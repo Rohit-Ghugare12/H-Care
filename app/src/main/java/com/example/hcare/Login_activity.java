d9db4747119f729c7a11c4cfb458d3fa409858b4
Gaurav_dev <gauravsolunke5@gmail.com>
2026-08-15 12:37:10 +0530

    new commit by gaurav

--- app/src/main/java/com/example/hcare/Login_activity.java
+++ app/src/main/java/com/example/hcare/Login_activity.java
@@ -1,24 +1,30 @@
 package com.example.hcare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class Login_activity extends AppCompatActivity {
+
+    TextView tvForgetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tvForgetPassword = findViewById(R.id.tvForgetPassword);
+
+        tvForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_activity.this, Home_Screen.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
