58a452f11724fb1de1f80e6553c7a28a095885b2
Rohit <rghugare720@gmail.com>
2026-08-15 11:36:46 +0600

    Action menu created and Bottom Navigation Created

---
+++ app/src/main/java/com/example/hcare/Home_Screen.java
@@ -0,0 +1,23 @@
+package com.example.hcare;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Home_Screen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home_screen);
+
+
+    }
+}
