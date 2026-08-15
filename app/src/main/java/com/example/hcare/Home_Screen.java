504b0b2c4ce5f34183cc778762cf91f5b639409a
Gaurav_dev <gauravsolunke5@gmail.com>
2026-08-15 16:38:29 +0530

    my camp backend

--- app/src/main/java/com/example/hcare/Home_Screen.java
+++ app/src/main/java/com/example/hcare/Home_Screen.java
@@ -1,13 +1,19 @@
 package com.example.hcare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.hcare.fragment.MyCampFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home_Screen extends AppCompatActivity {
@@ -22,6 +28,27 @@ public class Home_Screen extends AppCompatActivity {
 
         setContentView(R.layout.activity_home_screen);
 
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.home_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId()==R.id.bottomCamp)
+        {
+            Intent intent = new Intent(Home_Screen.this, MyCampFragment.class);
+            startActivity(intent);
+
+        }
+
+
+        return true;
+    }
 }
